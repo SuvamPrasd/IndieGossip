16ced8d876f43ca11f3334d58ba83fb9ed105069
SuvamPrasd <tituprasad30@gmail.com>
2020-05-04 10:20:26 +0530

    Updated main

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'IndieGossip',
+    home: new Scaffold(
+      appBar: AppBar(
+        title: Text('IndieGossip'),
+      ),
+    ),
+  ));
+}
